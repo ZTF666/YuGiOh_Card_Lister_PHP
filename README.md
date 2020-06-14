9df506097d6f5b6c5b229ca2589e1744927011ab
ZTF <ZTF666@protonmail.ch>
2020-06-14 09:10:10 +0100

    cleaner code , few adjustements and aesthetics

--- README.md
+++ README.md
@@ -1,29 +1,69 @@
-# YuGiOh_Card_Lister_PHP
+# YU GI OH | 遊 ☆ 戯 ☆ 王
 
-![Screenshot](imgs/logo.png)
+<div align="center">
+<img src="imgs/logo.png" >
+</div>
+
 A simple one page app that consumes the yugioh api and displays the details about the cards.
 The app uses the 'https://db.ygoprodeck.com/api-guide/' V5 api to fetch the data.
-![Screenshot](imgs/logo.png)
 
+<div align="center">
+<img src="https://firebasestorage.googleapis.com/v0/b/katzfacts.appspot.com/o/yugi.gif?alt=media&token=15bfd4c6-64b9-497c-8afb-a272466da7f6" >
+</div>
 
-## Empty List
-![Screenshot](imgs/1.png)
+## How does it work :
+
+```
+By Default the list of card displayed when the page is accessed are EXODIA's.
+This can be changed in the code to which ever card you want or tinker and diasble the list at the launch .
+```
+
+<div align="center">
+<img src="imgs/default.png" >
+</div>
 
 ## Card Detail
-![Screenshot](imgs/3.png)
 
-## Card Search
-![Screenshot](imgs/2.gif)
+```
+Click on **More Detail** to display the flavour text of the card .
+```
 
-## Project setup
+<div align="center">
+<img src="imgs/imgs/3.png" >
+</div>
+
+## Tips
+
+```php
+# in case of the error : Unable to find the wrapper “https”
+# enable this extension in the php.ini
+
+;extension=php_openssl
+
 ```
-You can run the page on any server( i guess ).
- This is what i used in my setup :
- Apache 2.4.25 - PHP 7.1.3 
- ```
- 
- ### Notice 
- ```
- You can use the project , modify it in any way you wish as long as you credit me !
- you can reach me @ZTF666@protonmail.ch
+
+## Contact
+
 ```
+you can contact me at ZTF666@protonmail.ch or via my portfolio
+
+```
+
+<div align="center">
+
+<table>
+  <tr>
+    <td align="center"><a href="https://ztfportfolio.web.app/" target='_blank'><img src="https://avatars1.githubusercontent.com/u/32502988?v=4" width="100px;" alt=""/><br /><sub><b>ZTF666</b></sub></a></td>
+  </tr>
+</table>
+
+</div>
+
+## License
+
+**💎YU GI OH | 遊 ☆ 戯 ☆ 王 💎** released under the [MIT](LICENSE) License.
+<br><br>
+
+<div align="center">
+<strong><p>Made with 💘 by a 👨‍💻 on a 💻 | 2020 | ZTF666 - N.EA</p> </strong>
+</div>
--- src/ZTFDeck.php
+++ src/ZTFDeck.php
@@ -1,7 +1,7 @@
 <?php
-ini_set( 'display_errors', 0 );
+ini_set('display_errors', 1);
 ?>
-<!-- This application was made by ZTF(Nabil elatif) , if you use it somewhere please credit me ! -->
+
 <!DOCTYPE html>
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
@@ -18,9 +18,8 @@ ini_set( 'display_errors', 0 );
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="shortcut icon" href="../imgs/logoz.ico" />
 
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <link rel="stylesheet" href="../CSS/Style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="../CSS/Style.css">
 
 </head>
 
@@ -28,7 +27,7 @@ ini_set( 'display_errors', 0 );
   <div class="outer">
     <div class="inner">
       <form method="post" action="">
-        
+
         <img src="../imgs/logo.png" class="logo">
         <input type="text" name="CARD" required placeholder="Card name or ID ">
       </form>
@@ -38,88 +37,100 @@ ini_set( 'display_errors', 0 );
   <div class="grid-container">
 
     <?php
-    //? by default the list will be filled by EXODIA set of cards , it was a personal pref/approach
-    //? but the list will be updated once a new card is searched O B V I O U S L Y lol
-    $name="exodia";
-    if(isset($_POST['CARD'])){
-    $name=rawurlencode($_POST['CARD']);}
-       
-        $json_string =    file_get_contents("https://db.ygoprodeck.com/api/v5/cardinfo.php?fname=".$name);
-        $parsed_json = json_decode($json_string, true);
-      //? tbh this should never occure but for good mesures i implemented it
-        if(!isset($parsed_json) && $_POST['search']){
-            echo '<script language="javascript">';
-            echo 'alert("Wait what ? this should never happen !!")';
-            echo '</script>';
-        }//? in case the string inserted in the search bar wasn't correct or doens't exist in the api's db
-        else if(!isset($parsed_json)){
-          echo '<script language="javascript">';
-          echo 'alert("The card does not exist ! please make sure you typed the right card name")';
-          echo '</script>';
-        }
-        else{
-        $att="";
 
-        foreach($parsed_json as $X ){ 
-            
-?>
-    <div class="grid-item">
-      <ul class="list-group">
-        <li class="list-group-item mylist"><img src="<?php echo $X['card_images'][0]['image_url'] ?>" width="250"
-            height="365"></li>
-        <li class="list-group-item mylist"><?php echo  $X['name'];?></li>
-        <li class="list-group-item mylist">
-          <?php if(isset($X['level'])){ echo $X['level'];echo " ";
-                echo "<img src='https://ygoprodeck.com/wp-content/uploads/2017/01/level.png' width='20' height='20'>";}else{echo 'This card has no level';} ?>
-        </li>
-        <li class="list-group-item mylist">
-          <?php if(isset($X['attribute'])){ echo $X['attribute'];}else{echo 'This card has no attribute';} ?></li>
-        <li class="list-group-item mylist"><?php echo  $X['race']; ?></li>
-        <li class="list-group-item mylist"><?php echo  $X['type']; ?></li>
-        <li class="list-group-item mylist">
-          <button type="button" class="btn btn-primary" data-toggle="modal"
-            data-target="#exampleModal<?php echo  $X['id']; ?>">
-            More Detail...
-          </button>
-        </li>
-      </ul>
-    </div>
-    <!-- Modal where details about the card is displayed -->
-    
-    <div class="modal fade" id="exampleModal<?php echo  $X['id']; ?>" tabindex="-1" role="dialog"
-      aria-labelledby="exampleModalLabel" aria-hidden="true" style=" color:black;">
-      <div class="modal-dialog" role="document">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel"><?php echo  $X['name'];?></h5>
-            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="modal-body">
-            <img src="<?php echo $X['card_images'][0]['image_url'] ?>" width="250" height="365"><br>
-            <?php echo  $X['desc']; ?>
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary btn-lg btn-block" data-dismiss="modal">Close</button>
+    $name = "exodia";
+    if (isset($_POST['CARD'])) {
+
+      $name = rawurlencode($_POST['CARD']);
+    }
+
+    $json_string =    file_get_contents("https://db.ygoprodeck.com/api/v5/cardinfo.php?fname=" . $name);
+    $parsed_json = json_decode($json_string, true);
+    //? tbh this should never occure but for good mesures i implemented it
+    if (!isset($parsed_json)) {
+      echo '<script language="javascript">';
+      echo 'alert("Wait what ? this should never happen !!")';
+      echo '</script>';
+    } //? in case the string inserted in the search bar wasn't correct or doens't exist in the api's db
+    else if (!isset($parsed_json)) {
+      echo '<script language="javascript">';
+      echo 'alert("The card does not exist ! please make sure you typed the right card name")';
+      echo ($_POST['CARD']);
+      echo '</script>';
+    } else {
+      $att = "";
+
+      foreach ($parsed_json as $X) {
+
+    ?>
+        <div class="grid-item">
+          <ul class="list-group">
+            <li class="list-group-item mylist"><img src="<?php echo $X['card_images'][0]['image_url'] ?>" width="250" height="365"></li>
+            <li class="list-group-item mylist"><?php echo  $X['name']; ?></li>
+            <li class="list-group-item mylist">
+              <?php if (isset($X['level'])) {
+                echo $X['level'];
+                echo " ";
+                echo "<img src='https://ygoprodeck.com/wp-content/uploads/2017/01/level.png' width='20' height='20'>";
+              } else {
+                echo 'This card has no level';
+              } ?>
+            </li>
+            <li class="list-group-item mylist">
+              <?php if (isset($X['attribute'])) {
+                echo $X['attribute'];
+              } else {
+                echo 'This card has no attribute';
+              } ?></li>
+            <li class="list-group-item mylist"><?php echo  $X['race']; ?></li>
+            <li class="list-group-item mylist"><?php echo  $X['type']; ?></li>
+            <li class="list-group-item mylist">
+              <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal<?php echo  $X['id']; ?>">
+                More Detail...
+              </button>
+            </li>
+          </ul>
+        </div>
+        <!-- Modal where details about the card is displayed -->
+
+        <div class="modal fade" id="exampleModal<?php echo  $X['id']; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" style=" color:black;">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel"><?php echo  $X['name']; ?></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <img src="<?php echo $X['card_images'][0]['image_url'] ?>" width="250" height="365"><br>
+                <?php echo  $X['desc']; ?>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary btn-lg btn-block" data-dismiss="modal">Close</button>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-    <?php }}?>
+    <?php }
+    } ?>
   </div>
   <!-- Copyright -->
+  <div id="side">
+    <div style="display:flex;justify-content:center;align-items:flex-end;">
+      <div>
+        <small id="tex">🌛 &copy; BlackHole🌌Studio LLC | Made with 💘 by an 👽 on a 💻 for
+          👾 | 2020 | E.A.N aka ZTF 🌜</small>
+      </div>
+    </div>
+  </div>
 
- <!-- This application was made by ZTF(Nabil elatif) , if you use it somewhere please credit me ! -->
+
 </body>
 
 <!-- Scripts -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-  integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-  integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-  integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
 </html>
-<!-- This application was made by ZTF(Nabil elatif) , if you use it somewhere please credit me ! -->
